cebacd0a2ded88fc3bff3077076fb09fe43d56cf
Eduardo Renz <dudarenz@gmail.com>
2023-04-21 10:20:51 -0300

    new tests

--- src/index.php
+++ src/index.php
@@ -1,3 +1,12 @@
 <?php
-echo "hi";
-echo phpinfo();
+
+require __DIR__ . '/../vendor/autoload.php';
+use pCloud\Api;
+
+// $client = new GuzzleHttp\Client();
+// $res = $client->request('GET', "https://my.pcloud.com/oauth2/authorize?client_id=VYHjjoAHDh0&response_type=string");
+
+// echo $res->getBody();
+
+$api = new Api();
+$api->authenticate('client_id', 'client_secret');
