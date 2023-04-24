552d6d283eba36ac49db4749801af10c00f98d7b
Eduardo Renz <dudarenz@gmail.com>
2023-04-23 21:39:03 -0300

    implemented file upload

--- src/GoogleDriveFileGateway.php
+++ src/GoogleDriveFileGateway.php
@@ -2,6 +2,9 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once './interfaces/FileGateway.php';
 
+use Google\Client;
+use Google\Service\Drive;
+
 define('DRIVE_BASE_ENDPONT', 'https://www.googleapis.com/drive/v3/');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
@@ -9,24 +12,64 @@ $dotenv->load();
 class GoogleDriveFileGateway implements FileGateway
 {
     private $folder;
-    private $httpClient;
     private $googleClient;
+    private $driveService;
 
 
     function __construct($folder = null)
     {
         $this->folder = $folder;
-        $this->httpClient = new  GuzzleHttp\Client(['base_uri' => DRIVE_BASE_ENDPONT]);
-
-        $this->googleClient = new Google_Client();
-        $this->googleClient->setAuthConfigFile(__DIR__ . '/keys/auth.json');
-        $this->googleClient->addScope(Google_Service_Drive::DRIVE);
+        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/keys/auth.json');
+        $this->googleClient = new Client();
+        $this->googleClient->addScope(Drive::DRIVE);
+        $this->googleClient->useApplicationDefaultCredentials();
+        $this->driveService = new Drive($this->googleClient);
     }
 
     public function getFiles()
     {
+        try {
+            $files = array();
+            $pageToken = null;
+            do {
+                $response = $this->driveService->files->listFiles(array(
+                    'q' => "mimeType='image/jpeg'",
+                    'spaces' => 'drive',
+                    'pageToken' => $pageToken,
+                    'fields' => 'nextPageToken, files(id, name)',
+                    'includeItemsFromAllDrives' => true
+                ));
+                foreach ($response->files as $file) {
+                    printf("Found file: %s (%s)\n", $file->name, $file->id);
+                }
+                array_push($files, $response->files);
 
-        $files = $this->httpClient->get('/files?key=teste');
-        return $files;
+                $pageToken = $response->pageToken;
+            } while ($pageToken != null);
+            return $files;
+        } catch (Exception $e) {
+            echo "Error Message: " . $e;
+        }
+    }
+
+    public function upload($file)
+    {
+        try {
+            $fileMetadata = new Drive\DriveFile(array(
+                'name' => $file['name'],
+                'parents' => ['1DUIqGuinrhQfNnyzKFBSZbuh9KW0ZVu5']
+            ));
+            $content = file_get_contents($file['tmp_name']);
+            $file = $this->driveService->files->create($fileMetadata, array(
+                'data' => $content,
+                'mimeType' => $file['type'],
+                'uploadType' => 'multipart',
+                'fields' => 'id',
+            ));
+            printf("File ID: %s\n", $file->id);
+            return $file->id;
+        } catch (Exception $e) {
+            echo "Error Message: " . $e;
+        }
     }
 }
