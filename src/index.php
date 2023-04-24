8d8c9fe17e9ad89e64faa0a1863429a51d1a7608
Eduardo Renz <dudarenz@gmail.com>
2023-04-23 21:39:10 -0300

    index is a form

--- src/index.php
+++ src/index.php
@@ -1,17 +1,52 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require __DIR__ . '/../vendor/autoload.php';
-include_once './GoogleDriveFileGateway.php';
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload</title>
+</head>
 
-$drive = new GoogleDriveFileGateway();
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name"><br><br>
+        <label for="movie">Movie:</label>
+        <input type="file" id="movie" name="movie"><br><br>
+        <label for="progress">Progress:</label>
+        <progress id="progress" value="0" max="100"></progress><br><br>
+        <input type="submit" value="Submit">
+    </form>
 
-$files = $drive->getFiles();
+    <script>
+        var form = document.querySelector('form');
+        var progress = document.querySelector('#progress');
 
-echo $files;
-// $client = new GuzzleHttp\Client();
-// $res = $client->request('GET', "https://my.pcloud.com/oauth2/authorize?client_id=VYHjjoAHDh0&response_type=string");
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            var formData = new FormData(form);
+            var xhr = new XMLHttpRequest();
 
-// echo $res->getBody();
+            xhr.upload.onprogress = function(e) {
+                if (e.lengthComputable) {
+                    var percent = Math.round((e.loaded / e.total) * 100);
+                    progress.value = percent;
+                }
+            };
 
-//$api = new Api();
-//$api->authenticate('client_id', 'client_secret');
+            xhr.onload = function() {
+                // Handle success response
+            };
+
+            xhr.onerror = function() {
+                // Handle error response
+            };
+
+            xhr.open('POST', 'upload.php', true);
+            xhr.send(formData);
+        });
+    </script>
+</body>
+
+</html>
