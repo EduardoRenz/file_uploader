84ae286b159027e404ecfb9c6e683157f63ea6fc
Eduardo Renz <dudarenz@gmail.com>
2023-04-23 21:39:24 -0300

    separated upload

---
+++ src/upload.php
@@ -0,0 +1,13 @@
+<?php
+@ini_set('upload_max_size', '2056M');
+@ini_set('post_max_size', '2056M');
+@ini_set('max_execution_time', '300');
+require __DIR__ . '/../vendor/autoload.php';
+include_once './GoogleDriveFileGateway.php';
+
+
+$drive = new GoogleDriveFileGateway();
+$upload = $drive->upload($_FILES['movie']);
+#$files = $drive->getFiles();
+
+echo "Success";
